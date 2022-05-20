2f9f5f9969db3b933cd72141406429f42fdd1e45
Zed Null <34741787+StarProxima@users.noreply.github.com>
2022-05-20 20:41:09 +0300

    ChatPage

---
+++ camp_app/lib/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = const Color(0xff257ADE);
+  static Color border = const Color(0xffEDEDED);
+  static Color background = const Color(0xffFFFFFF);
+  static Color textDark = const Color(0xff181818);
+}
